002098d37c808e0d00f0c5141f6caad6c3719dc5
Xixi <flyskyxi@gmail.com>
2016-01-21 02:22:26 -0500

    add remove modal window, window is working, but function have set up yet.

--- modules/courses/client/config/courses.client.routes.js
+++ modules/courses/client/config/courses.client.routes.js
@@ -24,6 +24,13 @@ angular.module('courses').config(['$stateProvider',
           roles: ['admin', 'professor'] //only user/professor and admins are allowed to manipulate course
         }
       })
+      .state('courses.remove', {
+        url: '/:courseId/remove',
+        templateUrl: 'modules/courses/client/views/remove-course.client.view.html',
+        data: {
+          roles: ['admin'] //only admin be able to remove course
+        }
+      })
       .state('courses.view', {
         url: '/:courseId',
         templateUrl: 'modules/courses/client/views/view-course.client.view.html',
