0f446fb2ad83f05887803b013de08bf6c52fc022
Xixi <flyskyxi@gmail.com>
2016-01-21 02:21:31 -0500

    fix the thing I break before...

--- modules/courses/client/controllers/list-courses.client.controller.js
+++ modules/courses/client/controllers/list-courses.client.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // Courses controller
-angular.module('courses').controller('CoursesListController', ['$scope', '$filter', 'Courses', '$modal', '$log',
-  function ($scope, $filter, Courses, $modal, $log) {
+angular.module('courses').controller('CoursesListController', ['$state', 'Users','Authentication', '$scope', '$filter', 'Courses', '$modal', '$log',
+  function ($state, Users, Authentication, $scope, $filter, Courses, $modal, $log) {
+    
+
+
     Courses.query(function (data) {
       $scope.courses = data;
       $scope.buildPager();
@@ -29,57 +32,125 @@ angular.module('courses').controller('CoursesListController', ['$scope', '$filte
       $scope.figureOutItemsToDisplay();
     };
 
+        // Open a modal window to update a single course record
+    $scope.modalUpdate = function(size, selectedCourse) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/courses/client/views/edit-course.client.view.html',
+        controller: ModalUpdateCtrl,
+        size: size,
+        resolve: {
+          course: function() {
+            return selectedCourse;
+          }
+        }
+      });
+
+      modalInstance.result.then(function(selectedItem) {
+        $scope.selected = selectedItem;
+      }, function() {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    var ModalUpdateCtrl = function($scope, $modalInstance, course) {
+      $scope.course = course;
+
+      $scope.ok = function() {
+        $modalInstance.close($scope.course);
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+        $state.reload();
+      };
+    };
+
+        // Open a modal window to Remove a single course record
+    $scope.modalRemove = function(size, selectedCourse) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'modules/courses/client/views/remove-course.client.view.html',
+        controller: ModalRemoveCtrl,
+        size: size,
+        resolve: {
+          course: function() {
+            return selectedCourse;
+          }
+        }
+      });
+
+      modalInstance.result.then(function(selectedItem) {
+        $scope.selected = selectedItem;
+      }, function() {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
+    var ModalRemoveCtrl = function($scope, $modalInstance, course) {
+      $scope.course = course;
+
+      $scope.ok = function() {
+        $modalInstance.close($scope.course);
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+        $state.reload();
+      };
+    };
+
     
-    // Open a modal window to View a single course record
-        $scope.modalView = function(size, selectedCourse) {
+  //   // Open a modal window to View a single course record
+  //   $scope.modalView = function(size, selectedCourse) {
 
-            var modalFlag = true;
+  //           var modalFlag = true;
 
-            if (Authentication.user) {
-                if (Authentication.user.roles.indexOf('instructor') > -1 ||
-                    Authentication.user.roles.indexOf('admin') > -1) {
-                    modalFlag = false;
-                    $location.path('course/' + selectedCourse._id);
-                } else {
-                    for (var i = 0; i < Authentication.user.coursesPurchased.length; i++) {
-                        if (Authentication.user.coursesPurchased[i].courseId == selectedCourse._id) {
-                            modalFlag = false;
-                            $location.path('course/' + selectedCourse._id);
-                        }
-                    }
-                }
-            }
-            if (modalFlag) {
-                var modalInstance = $modal.open({
-                    templateUrl: 'modules/courses/client/views/view-course.client.view.html',
-                    controller: ModalViewCtrl,
-                    size: size,
-                    resolve: {
-                        course: function() {
-                            return selectedCourse;
-                        }
-                    }
-                });
+  //           if (Authentication.user) {
+  //               if (Authentication.user.roles.indexOf('instructor') > -1 ||
+  //                   Authentication.user.roles.indexOf('admin') > -1) {
+  //                   modalFlag = false;
+  //                   $location.path('course/' + selectedCourse._id);
+  //               } else {
+  //                   for (var i = 0; i < Authentication.user.enrolledCourses.length; i++) {
+  //                       if (Authentication.user.enrolledCourses[i].courseId === selectedCourse._id) {
+  //                           modalFlag = false;
+  //                           $location.path('course/' + selectedCourse._id);
+  //                       }
+  //                   }
+  //               }
+  //           }
+  //           if (modalFlag) {
+  //               var modalInstance = $modal.open({
+  //                   templateUrl: 'modules/courses/client/views/view-course.client.view.html',
+  //                   controller: ModalViewCtrl,
+  //                   size: size,
+  //                   resolve: {
+  //                       course: function() {
+  //                           return selectedCourse;
+  //                       }
+  //                   }
+  //               });
 
-                modalInstance.result.then(function(selectedItem) {
-                    $scope.selected = selectedItem;
-                }, function() {
-                    $log.info('Modal dismissed at: ' + new Date());
-                });
-            }
-        };
+  //               modalInstance.result.then(function(selectedItem) {
+  //                   $scope.selected = selectedItem;
+  //               }, function() {
+  //                   $log.info('Modal dismissed at: ' + new Date());
+  //               });
+  //           }
+  //   };
 
-        var ModalViewCtrl = function($scope, $modalInstance, course) {
-            $scope.course = course;
+  //   var ModalViewCtrl = function($scope, $modalInstance, course) {
+  //           $scope.course = course;
 
-            $scope.ok = function() {
-                $modalInstance.close($scope.course);
-            };
+  //           $scope.ok = function() {
+  //               $modalInstance.close($scope.course);
+  //           };
 
-            $scope.cancel = function() {
-                $modalInstance.dismiss('cancel');
-            };
-        };
+  //           $scope.cancel = function() {
+  //               $modalInstance.dismiss('cancel');
+  //           };
+  //       };
 
   }
 ]);
