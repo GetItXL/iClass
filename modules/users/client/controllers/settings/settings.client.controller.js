e22b675d0ff66b3243ed5e9ef620626de19bc401
Qianwen Ding <lulu.dingq@gmail.com>
2016-01-23 03:22:13 -0500

    modified user profile dropdown. lets hope that i can fall into sleep this time..

--- modules/users/client/controllers/settings/settings.client.controller.js
+++ modules/users/client/controllers/settings/settings.client.controller.js
@@ -3,5 +3,9 @@
 angular.module('users').controller('SettingsController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     $scope.user = Authentication.user;
+
+    $scope.isAdmin = function(){
+      return ($scope.authentication.user.roles.indexOf('admin') > -1);
+    };
   }
 ]);
