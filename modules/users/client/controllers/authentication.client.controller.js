6be57119cdc51cc081b92f7c2fb5c74fe71585a4
Qianwen Ding <lulu.dingq@gmail.com>
2016-01-29 02:14:57 -0500

    Fixed role bug when user sign up. Now we can fix tests

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -23,15 +23,18 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
       }
 
       $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
+
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
+
+        console.log('who am i?' + $scope.authentication.user);
 
         angular.element('#main-content').css({
                 'margin-left': '210px'
         });
         // And redirect to the previous or home page
         //$state.go($state.previous.state.name || 'home', $state.previous.params);
-         if($scope.authentication.user.roles.indexOf('admin') > -1)
+        if($scope.authentication.user.roles.indexOf('admin') > -1)
           $location.path('/admindashboard/users');
         else if($scope.authentication.user.roles.indexOf('professor') > -1)
           $location.path('/professordashboard');
